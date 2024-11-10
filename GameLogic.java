8e7ce74e07f66f097500c534c406854cfb9ca15f
e10withadot <e10withadot@gmail.com>
2024-11-10 15:37:21 +0200

    Added GameLogic.java placeholder     אורי- אתה יכול לעזור עם המטודות הללו

---
+++ GameLogic.java
@@ -0,0 +1,77 @@
+import java.util.List;
+
+public class GameLogic implements PlayableLogic {
+
+    @Override
+    public boolean locate_disc(Position a, Disc disc) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'locate_disc'");
+    }
+
+    @Override
+    public Disc getDiscAtPosition(Position position) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getDiscAtPosition'");
+    }
+
+    @Override
+    public int getBoardSize() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getBoardSize'");
+    }
+
+    @Override
+    public List<Position> ValidMoves() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'ValidMoves'");
+    }
+
+    @Override
+    public int countFlips(Position a) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'countFlips'");
+    }
+
+    @Override
+    public Player getFirstPlayer() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getFirstPlayer'");
+    }
+
+    @Override
+    public Player getSecondPlayer() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getSecondPlayer'");
+    }
+
+    @Override
+    public void setPlayers(Player player1, Player player2) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setPlayers'");
+    }
+
+    @Override
+    public boolean isFirstPlayerTurn() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isFirstPlayerTurn'");
+    }
+
+    @Override
+    public boolean isGameFinished() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isGameFinished'");
+    }
+
+    @Override
+    public void reset() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'reset'");
+    }
+
+    @Override
+    public void undoLastMove() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'undoLastMove'");
+    }
+    
+}
