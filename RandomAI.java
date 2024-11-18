022fc606ddf6ab4a544be708be8926fda1ff6e15
e10withadot <e10withadot@gmail.com>
2024-11-18 17:08:20 +0200

    Added dummy classes inheriting from Player     This is necessary for the game to run

---
+++ RandomAI.java
@@ -0,0 +1,12 @@
+public class RandomAI extends AIPlayer {
+    
+    public RandomAI(boolean b) {
+        super(b);
+    }
+    @Override
+    public Move makeMove(PlayableLogic gameStatus) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'makeMove'");
+    }
+
+}
