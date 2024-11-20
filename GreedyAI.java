4ee542718500177b83acd472fc484299cee911a9
orironen <oriorking@gmail.com>
2024-11-20 12:30:12 +0200

    made it worst

--- GreedyAI.java
+++ GreedyAI.java
@@ -7,15 +7,16 @@ public class GreedyAI extends AIPlayer {
     }
     @Override
     public Move makeMove(PlayableLogic gameStatus) {
+        Disc disc = new SimpleDisc(this);
         int max_flip =0;
-        // search the move with the most fli;
+        // search the move with most flip;
         List<Position> moves =  gameStatus.ValidMoves();
         for (int i = 0; i < moves.size(); i++) {
             if (gameStatus.countFlips(moves.get(i))>max_flip){
                 max_flip = gameStatus.countFlips(moves.get(i));
             }
         }
-
+        return new Move(moves.get(max_flip), disc);
     }
 
 }
