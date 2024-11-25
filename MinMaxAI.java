d949787f0972d62d4d9d59de0e094bed0ea4dc62
e10withadot <e10withadot@gmail.com>
2024-11-25 15:11:57 +0200

    added placeholder MinMaxAI class

---
+++ MinMaxAI.java
@@ -0,0 +1,12 @@
+public class MinMaxAI extends AIPlayer {
+    public MinMaxAI(boolean isPlayerOne){
+        super(isPlayerOne);
+    }
+
+    @Override
+    public Move makeMove(PlayableLogic gameStatus) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'makeMove'");
+    }
+
+}
